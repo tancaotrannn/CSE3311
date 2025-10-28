7eaa290fb9af31980f5ef131101de5797bbce305
thomstah <thommyxay@gmail.com>
2025-10-27 19:51:14 -0500

    next.js eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginNext from "@next/eslint-plugin-next";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
+    plugins: { js, React: pluginReact, "@next/next": pluginNext },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
+    },
   },
   pluginReact.configs.flat.recommended,
 ]);
