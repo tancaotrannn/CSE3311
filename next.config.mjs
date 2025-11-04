d91c496518617fef1b7e1a48881f9ab118da3df1
thomstah <thommyxay@gmail.com>
2025-11-04 00:45:09 -0600

    SHOULD WORK NOW LLOLOLOL

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = "CSE3311";
+
 const nextConfig = {
   output: "export",
+  assetPrefix: "/" + repo + "/",
+  basePath: "/" + repo,
   trailingSlash: true,
   images: {
-    loader: "akamai", 
-    path: ""  
+    loader: "akamai",
+    path: "",
   },
-  assetPrefix: "./",   
- };
+};
 
 export default nextConfig;
