0894b2094cc367ffcccee4c545867e3130848e92
thomstah <thommyxay@gmail.com>
2025-11-04 00:30:23 -0600

    /out directory created

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,21 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import pluginNext from "@next/eslint-plugin-next";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+import nextVitals from "eslint-config-next/core-web-vitals";
+import next from "next";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-front": "off",
+    },
+  },
+  globalIgnores([".next/**", , "out/**", "build/**", "next-env.d.ts"]),
+]);
 
 export default defineConfig([
   {
@@ -15,10 +29,14 @@ export default defineConfig([
         version: "detect",
       },
     },
+    ...nextVitals,
     rules: {
       ...pluginNext.configs.recommended.rules,
       ...pluginNext.configs["core-web-vitals"].rules,
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-front": "off",
     },
   },
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   pluginReact.configs.flat.recommended,
 ]);
