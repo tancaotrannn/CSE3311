be03360411bf4e6ee04b43bc4d433266936b027b
Tanthony Caotran <142858206+tancaotrannn@users.noreply.github.com>
2025-11-03 23:20:03 -0600

    Reverting changes to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/CSE3311',
-  assetPrefix: '/CSE3311/',
-};
 
 export default nextConfig;
